f542db8d38a776a8321c1f7d7e248951a3312ca1
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-01-21 16:04:10 -0500

    boss updated position

--- src/Entity/Boss.java
+++ src/Entity/Boss.java
@@ -1,5 +1,6 @@
 package Entity;
 
+import java.util.ArrayList;
 import Tiles.TileMap;
 
 public class Boss extends MapObject {
@@ -8,6 +9,11 @@ public class Boss extends MapObject {
     private int maxHealth;
     private boolean dead;
 	private int damage;
+
+    private ArrayList<Money> projectiles;
+    private int projectileDamage;
+
+    private long projectileTimerCounter;
 	
 
     public Boss(TileMap tm) {
@@ -16,6 +22,59 @@ public class Boss extends MapObject {
         health = maxHealth = 20;
 		damage = 1;
 
+        projectiles = new ArrayList<Money>();
+        projectileTimerCounter = System.nanoTime();
+    }
+
+    private void getNextPosition() {
+		
+		// movement
+		if(left) {
+			dx -= moveSpeed;
+			if(dx < -maxSpeed) {
+				dx = -maxSpeed;
+			}
+		}
+		else if(right) {
+			dx += moveSpeed;
+			if(dx > maxSpeed) {
+				dx = maxSpeed;
+			}
+		}
+		
+	}
+
+    public void update(){
+        getNextPosition();
+		checkTileMapCollision();
+		setPosition(xtemp, ytemp);
+        
+        long elapsed = (System.nanoTime() - projectileTimerCounter) / 1000000;
+
+        if (elapsed > 5000) {
+            Money projectile = new Money(tileMap,facingRight);
+            projectile.setPosition(x, y+20);
+            projectiles.add(projectile);
+            projectile.setPosition(x, y);
+            projectiles.add(projectile);
+            projectile.setPosition(x, y-20);
+            projectiles.add(projectile);
+            projectileTimerCounter = System.nanoTime();
+        }
+
+        for (int i = 0; i < projectiles.size(); i++) {
+            projectiles.get(i).update();
+            if (projectiles.get(i).undrawFromGame()) {
+                projectiles.remove(i);
+                i--;
+            }
+        }
+
+    }
+
+
+    public void draw(){
+        
     }
     
 }
